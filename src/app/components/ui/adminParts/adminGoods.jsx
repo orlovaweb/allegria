230d2e2e77733cb1f47e3da13e17838bbb0194f7
Orlova <igimon@mail.ru>
2024-08-13 09:53:33 +0300

    add some styles for admin goods

--- src/app/components/ui/adminParts/adminGoods.jsx
+++ src/app/components/ui/adminParts/adminGoods.jsx
@@ -8,35 +8,38 @@ const AdminGoods = () => {
   const goods = useSelector(getGoods());
   if (goods) {
     return (
-      <section className="goods-table">
-        <div className="product-header">Арт</div>
-        <div className="product-header">Название</div>
-        <div className="product-header">Цена</div>
-        <div className="product-header">Скидка</div>
-        <div className="product-header">Размер</div>
-        <div className="product-header">Остаток</div>
-        <div className="product-header">Редактировать/Удалить</div>
-        {goods.map((product) => (
-          // <div className="product-row" key={product._id}>
-          <>
-            <div className="product-art">{product.art}</div>
-            {/* <div className="product-art">...{product.art.slice(-4)}</div> */}
-            <div className="product-name">{product.name}</div>
-            <div className="product-price">{product.price}</div>
-            <div className="product-discount">{product.discount}</div>
-            <div className="product-size">
-              {product.size && <AdminSize sizeId={product.size} />}
-            </div>
-            <div className="product-count">{product.count}</div>
-            <div className="product-delete">
-              <IconEdit />
-              <IconDelete />
-            </div>
-          </>
-        ))}
+      <>
+        <section className="goods-table">
+          <div className="product-header">Арт</div>
+          <div className="product-header">Название</div>
+          <div className="product-header">Цена</div>
+          <div className="product-header">Скидка</div>
+          <div className="product-header">Размер</div>
+          <div className="product-header">Остаток</div>
+          <div className="product-header">Редактировать/Удалить</div>
+          {goods.map((product) => (
+            // <div className="product-row" key={product._id}>
+            <>
+              <div className="product-art">{product.art}</div>
+              {/* <div className="product-art">...{product.art.slice(-4)}</div> */}
+              <div className="product-name">{product.name}</div>
+              <div className="product-price">{product.price}</div>
+              <div className="product-discount">{product.discount}</div>
+              <div className="product-size">
+                {product.size && <AdminSize sizeId={product.size} />}
+              </div>
+              <div className="product-count">{product.count}</div>
+              <div className="product-delete">
+                <IconEdit />
+                <IconDelete />
+              </div>
+            </>
+          ))}
 
-        <p>Всего {goods.length}</p>
-      </section>
+          <p>Всего {goods.length}</p>
+        </section>
+        <button className="btn">Добавить товар</button>
+      </>
     );
   } else return <p>Loading...</p>;
 };
